8610878a3f91064273a6e37058c946d826fc2e0e
AminRafifFebrian11 <49119429+AminRafifFebrian11@users.noreply.github.com>
2019-10-16 15:30:34 +0700

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,20 @@
+import tabung
+
+def main():
+
+	volume = tabung.volume(50,20)
+
+	print("Volume dari r=50 dan t=20 = " , volume)
+
+	luasSelimut = tabung.luasSelimut(60,10)
+
+	print("luas Selimut dari r=60 dan t=10 = " , luasSelimut)
+
+	
+	kelilingAlas = tabung.kelilingAlas(55)
+
+	print("keliling alas r=55 = " , kelilingAlas)
+
+
+if __name__=="__main__":
+	main()
